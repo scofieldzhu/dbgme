4dec4d9586eaa12b51e5dc03ed89fd01c836fd12
scofieldzhu <scofieldzhu@ymail.com>
2015-09-13 21:21:49 +0800

    update config file format

--- master/src/sflogger/loggerParser.h
+++ master/src/sflogger/loggerParser.h
@@ -1,11 +1,17 @@
 #ifndef __LOGGER_PARSER_H__
 #define __LOGGER_PARSER_H__
 
+#include "langext.h"
+
 class LoggerParser
 {
 public:
-    LoggerParser();
+    LoggerParser(const std::xStrT& conf_file);
+    bool Parse();
     ~LoggerParser();
+
+private:
+    std::xStrT conf_file_;
 };
 
 #endif
