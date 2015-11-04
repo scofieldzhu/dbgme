78924faa1d11d1306d3062a92b7f3da984f3bd1a
scofieldzhu <scofieldzhu@ymail.com>
2015-11-04 22:05:33 +0800

    revert

--- master/builds/vs2015_depoly.bat
+++ master/builds/vs2015_depoly.bat
@@ -1,23 +1,6 @@
 @echo off
 echo --------------->Rebuild Solution projectconfig:"Debug|Win32" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/dgr2/dgr2.vcxproj" /projectconfig "Debug|Win32" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/xmlConfKit/xmlConfKit.vcxproj" /projectconfig "Debug|Win32" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/test/test.vcxproj" /projectconfig "Debug|Win32" 
-
-echo --------------->Rebuild Solution projectconfig:"Release|Win32" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/dgr2/dgr2.vcxproj" /projectconfig "Release|Win32" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/xmlConfKit/xmlConfKit.vcxproj" /projectconfig "Release|Win32" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/test/test.vcxproj" /projectconfig "Release|Win32" 
-
-echo --------------->Rebuild Solution projectconfig:"Debug|x64" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/dgr2/dgr2.vcxproj" /projectconfig "Debug|x64" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/xmlConfKit/xmlConfKit.vcxproj" /projectconfig "Debug|x64" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/test/test.vcxproj" /projectconfig "Debug|x64" 
-
-echo --------------->Rebuild Solution projectconfig:"Release|x64" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/dgr2/dgr2.vcxproj" /projectconfig "Release|x64" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/xmlConfKit/xmlConfKit.vcxproj" /projectconfig "Release|x64" 
-devenv "VS/dbgme.sln" /rebuild Debug /project "VS/test/test.vcxproj" /projectconfig "Release|x64" 
+MsBuild.exe VS/dbgme.sln /t:rebuild
 
 echo --------------->Build Finished!
 
