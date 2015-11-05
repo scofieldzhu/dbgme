fc96c10818492b77eff228589ae5a509406fb716
scofieldzhu <scofieldzhu@ymail.com>
2015-11-05 22:26:47 +0800

    regenerate dist!

--- master/builds/vs2015_depoly.bat
+++ master/builds/vs2015_depoly.bat
@@ -1,41 +1,41 @@
 @echo off
 echo --------------->Rebuild Solution projectconfig:"Debug|Win32" 
-msbuild VS/dbgme.sln /p:configuration=debug /p:platform="x86"
+msbuild VS2015/dbgme.sln /p:configuration=debug /p:platform="x86" /t:rebuild
 
 echo --------------->Rebuild Solution projectconfig:"Debug|x64" 
-msbuild VS/dbgme.sln /p:configuration=debug /p:platform="x64"
+msbuild VS2015/dbgme.sln /p:configuration=debug /p:platform="x64" /t:rebuild
 
 echo --------------->Rebuild Solution projectconfig:"Release|Win32"
-msbuild VS/dbgme.sln /p:configuration=release /p:platform="x86"
+msbuild VS2015/dbgme.sln /p:configuration=release /p:platform="x86" /t:rebuild
 
 echo --------------->Rebuild Solution projectconfig:"Release|x64"
-msbuild VS/dbgme.sln /p:configuration=release /p:platform="x64"
+msbuild VS2015/dbgme.sln /p:configuration=release /p:platform="x64" /t:rebuild
 
 echo --------------->Copy folders and files 
 rd /s /Q "./../dist/"
 md .\..\dist\dbgme\include
 md .\..\dist\dbgme\libs
-xcopy VS\_bin\*.lib ..\dist\dbgme\libs /d /e /c /q /k /y /i
-xcopy VS\_bin\*.dll ..\dist\dbgme\libs /d /e /c /q /k /y /i
+xcopy VS2015\_bin\*.lib ..\dist\dbgme\libs /d /e /c /q /k /y /i
+xcopy VS2015\_bin\*.dll ..\dist\dbgme\libs /d /e /c /q /k /y /i
 xcopy ..\src\dgr2\*.h ..\dist\dbgme\include /d /e /c /q /k /y /i 
 xcopy ..\src\dgr2\*.hpp ..\dist\dbgme\include /d /e /c /q /k /y /i 
 rd /s /Q ..\dist\dbgme\include\sqlite-amalgamation
 xcopy ..\src\xmlConfKit\*.h ..\dist\dbgme\include\xmlConfKit /d /e /c /q /k /y /i
 xcopy ..\src\xmlConfKit\*.hpp ..\dist\dbgme\include\xmlConfKit /d /e /c /q /k /y /i
-rd /s /Q "VS/_bin/"
+rd /s /Q "VS2015/_bin/"
 
 echo ---------------->clean build output files
-rd /s /Q "VS/test/Debug/"
-rd /s /Q "VS/test/Release/"
-rd /s /Q "VS/test/x64/"
+rd /s /Q "VS2015/test/Debug/"
+rd /s /Q "VS2015/test/Release/"
+rd /s /Q "VS2015/test/x64/"
 
-rd /s /Q "VS/dgr2/Debug/"
-rd /s /Q "VS/dgr2/Release/"
-rd /s /Q "VS/dgr2/x64/"
+rd /s /Q "VS2015/dgr2/Debug/"
+rd /s /Q "VS2015/dgr2/Release/"
+rd /s /Q "VS2015/dgr2/x64/"
 
-rd /s /Q "VS/xmlConfKit/Debug/"
-rd /s /Q "VS/xmlConfKit/Release/"
-rd /s /Q "VS/xmlConfKit/x64/"
+rd /s /Q "VS2015/xmlConfKit/Debug/"
+rd /s /Q "VS2015/xmlConfKit/Release/"
+rd /s /Q "VS2015/xmlConfKit/x64/"
 
 echo --------------->Deploy dbgme finish!
 pause
